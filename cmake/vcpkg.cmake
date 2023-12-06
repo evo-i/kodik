2abdc6a6967344aaf4d4557db34d947db67721e7
evo <149657734+evo-i@users.noreply.github.com>
2023-12-06 09:25:18 +0300

    Remove cmake folder with vcpkg.cmake file

--- cmake/vcpkg.cmake
+++
@@ -1,4 +0,0 @@
-set (VCPKG_BOOTSTRAP_OPTIONS "-disableMetrics")
-option (VCPKG_MANIFEST_MODE "" ON)
-set (CMAKE_TOOLCHAIN_FILE
-  "${CMAKE_SOURCE_DIR}/vendor/vcpkg/scripts/buildsystems/vcpkg.cmake")
