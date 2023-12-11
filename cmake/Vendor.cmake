6f904786f7d889b61b3d22aab40df767f0ec691c
evo <149657734+evo-i@users.noreply.github.com>
2023-12-11 17:21:43 +0300

    Move submodules to cmake/Vendor.cmake file.

---
+++ cmake/Vendor.cmake
@@ -0,0 +1,14 @@
+function (kodik_vendor_nng)
+  set (NNG_TESTS OFF CACHE INTERNAL "Disable nng testing")
+  set (NNG_ENABLE_NNGCAT OFF CACHE INTERNAL "Disable NNG cat.")
+  add_subdirectory ("vendor/nng" EXCLUDE_FROM_ALL)
+endfunction (kodik_vendor_nng)
+
+function (kodik_vendor_munit)
+  add_subdirectory ("vendor/munit" EXCLUDE_FROM_ALL)
+endfunction (kodik_vendor_munit)
+
+function (kodik_vendor_json)
+  set (BUILD_TESTING OFF CACHE INTERNAL "Disable json-c testing")
+  add_subdirectory ("vendor/json-c" EXCLUDE_FROM_ALL)
+endfunction (kodik_vendor_json)
