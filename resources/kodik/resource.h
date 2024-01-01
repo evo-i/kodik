c9887f43faa6e23c4d49c1941f2971a68e664a6e
evo <149657734+evo-i@users.noreply.github.com>
2024-01-01 19:10:43 +0300

    Added new bundle of certificates of Mozilla. Updated utest submodule. Fixed error in 'resource.rc'.

--- resources/kodik/resource.h
+++ resources/kodik/resource.h
@@ -1,21 +1,13 @@
 #ifndef KODIK_RESOURCE_H
 #define KODIK_RESOURCE_H 1
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #define KODIK_IDR_CERTIFICATES_BUNDLE       1000
-#define KODIK_IDR_CERTIFICATES_TRUST_BUNDLE 1001
+#define KODIK_IDR_CERTIFICATES_BUNDLE_TRUST 1001
+#define KODIK_IDR_CERTIFICATES_BUNDLE_MZ    1002
 
 #define KODIK_IDS_DOMAIN_NAME               2000
 #define KODIK_IDS_LIBRARY_NAME              2001
 #define KODIK_IDS_LIBRARY_AUTHOR            2002
 #define KODIK_IDS_LIBRARY_URL               2003
-
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif /* KODIK_RESOURCE_H */
